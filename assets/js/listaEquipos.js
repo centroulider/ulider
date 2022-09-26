bd26e159d5f5bec1bcd7384da49c2f886f728c1a
centroulider <108536980+centroulider@users.noreply.github.com>
2022-09-26 09:02:03 -0500

    Update listaEquipos.js

--- assets/js/listaEquipos.js
+++ assets/js/listaEquipos.js
@@ -6,7 +6,7 @@ fetch('./assets/data/dataEquipos.json')
         div += `
         <div class="causes-item my-3">
             <div class="causes-img">
-                <a href="./programas/dts/equipo.html?idEquipo=${equipo.idteam}"><img src="assets/img/equipos/dts${equipo.idteam}/DTS${equipo.idteam}-1.jpg" title="${equipo.tipo} ${equipo.nombre}" alt="${equipo.tipo} ${equipo.nombre}"></a>
+                <a href="./programas/dts/equipo.html?idEquipo=${equipo.idteam}"><img src="assets/img/equipos/DTS${equipo.idteam}/DTS${equipo.idteam}-1.jpg" title="${equipo.tipo} ${equipo.nombre}" alt="${equipo.tipo} ${equipo.nombre}"></a>
             </div>
             <div class="causes-text my-4">
                 <a href="./programas/dts/equipo.html?idEquipo=${equipo.idteam}" class="d-flex"><img src="assets/img/equipos/dts${equipo.idteam}/dts${equipo.idteam}logo.jpg" alt="${equipo.nombre}" title="${equipo.nombre}" class="rounded-circle mr-2" style="width: 20px; height: 20px;"> <h3>${equipo.tipo} <span class="text-warning">${equipo.nombre}</span></h3></a>
